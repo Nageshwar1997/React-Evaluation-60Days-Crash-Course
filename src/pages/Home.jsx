380ec716f6c6af975c85113288f0ff1d9b5c1bf9
Nageshwar <nageshpawarpatil@gmail.com>
2024-06-02 17:59:05 +0530

    Home Page Added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,44 @@
-import { Box } from "@chakra-ui/react"
+import { Alert, Box, Spinner } from "@chakra-ui/react";
+import axios from "axios";
+import { useEffect } from "react";
+import { useState } from "react";
 
 const Home = () => {
-  return (
-    <Box>
-      
-    </Box>
-  )
-}
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
 
-export default Home
+  useEffect(() => {
+    const fetchProducts = async () => {
+      setLoading(true);
+      try {
+        let resp = await axios.get(
+          `https://dbioz2ek0e.execute-api.ap-south-1.amazonaws.com/mockapi/get-products`
+        );
+        setProducts(resp.data);
+        setLoading(false);
+        setError(false);
+      } catch (error) {
+        setLoading(false);
+        setError(true);
+      }
+    };
+
+    fetchProducts();
+  }, []);
+  return (
+    <Box p="4">
+      {loading ? (
+        <Spinner size="xl" />
+      ) : error ? (
+          <Alert status="error">
+            
+        </Alert>
+      ) : (
+        ""
+      )}
+    </Box>
+  );
+};
+
+export default Home;
